0ae39b30dc0a7ca5071bdf3d9a3b8535afdc103a
Cyoce <masterryangp@gmail.com>
2015-10-14 11:55:24 -0700

    first instance of JSprite constructor

--- Sprite.js
+++ Sprite.js
@@ -1,3 +1,12 @@
-document.body.innerHTML =
-"<script type='text/javascript' src='Program.js'></script>"
-+ document.body.innerHTML;
+function JSprite (init) {
+  var newClass = function f() {
+    f.init.apply(this, arguments);
+  };
+  newClass.init = init;
+  return newClass;
+}
+Object.defineProperty(JSprite,'frame',{
+  set:function (v) {
+    JSprite.canvas = new fabric.Canvas(v);
+  }
+});
