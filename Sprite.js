83443c9ad459c79a991312189d58ac437b399de6
Cyoce <masterryangp@gmail.com>
2015-10-27 12:13:01 -0700

    Added math functions

--- Sprite.js
+++ Sprite.js
@@ -1,11 +1,12 @@
 var JSprite = function jsp (init,img) {
+  var math = jsp.math;
   function proto (obj,name,val) {
     obj.prototype[name] = val;
   }
   function prop () {
-    Object.defineProperty.apply(this,arguments);
+    Object.defineProperty.apply(this, arguments);
   }
-  var id = ref => document.getElementById(ref);
+  var id  = ref => document.getElementById(ref);
   var out = function self () { //constructor function for sprite object
     this.rawx = 0;
     this.rawy = 0;
@@ -13,30 +14,39 @@ var JSprite = function jsp (init,img) {
     this.img = img || 'tinyplatypus';
     self.img = this.img;
     this.raw = new fabric.Image(id(this.img));
+    this.x = this.y = this.angle = 0;
     self.init.apply(this, arguments);
     self.clones.push(this);
-    jsp.canvas.add(this.raw);
+    jsp.frame.add(this.raw);
   };
   prop(out.prototype,'x',{
-    get:() => this.rawx,
+    get:function () {
+      return this.rawx;
+    },
     set:function (val) {
       this.rawx = val;
-      if(this.raw) this.raw.set('left',jsp.canvas.width/2 + (val - jsp.math.sin(this.angle) * this.raw.width/2));
+      var newx = this.raw.width/2
+      if(this.raw) this.raw.set('left',jsp.frame.width/2 + (val - newx));
       jsp.render;
     }
   });
   prop(out.prototype,'y',{
-    get:() => this.rawy,
+    get:function () {
+      return this.rawy;
+    },
     set:function (val) {
       this.rawy = val;
-      if(this.raw) this.raw.set('top',jsp.canvas.height/2 - (val + this.raw.height * 0.5));
+      var newy = this.raw.height / 2;
+      if(this.raw) this.raw.set('top',jsp.frame.height/2 - (val + newy));
       jsp.render;
     }
   });
   prop(out.prototype,'angle',{
-    get:()=> this.rawangle,
+    get:function () {
+      return this.rawangle;
+    },
     set: function (val) {
-      this.rawangle = val;
+      this.rawangle = (360 + val) % 360;
       this.raw.set('angle',val);
       jsp.render;
     }
@@ -55,16 +65,27 @@ var JSprite = function jsp (init,img) {
     this.y = y;
   });
   proto(out,'add',function () {
-    jsp.canvas.add(this.raw);
-  })
+    jsp.frame.add(this.raw);
+  });
   return out;
 }
 Object.defineProperty(JSprite,'frame',{
-  set:v => JSprite.canvas = new fabric.Canvas(v)
+  get: _ => JSprite.canvas,
+  set: v => JSprite.canvas = new fabric.Canvas(v)
 });
 Object.defineProperty(JSprite,'render',{
-  get:() => Sprite.canvas.renderAll()
+  get:function () {
+    JSprite.frame.renderAll();
+  }
 });
 JSprite.math = {
-  sin: angle => Math.sin(angle * Math.PI/180)
+  deg:  radians => radians * 180 / Math.PI,
+  rad:  degrees => degrees * Math.PI / 180,
+  sin:  angle   => Math.sin(JSprite.math.rad(angle)),
+  cos:  angle   => Math.cos(JSprite.math.rad(angle)),
+  tan:  angle   => Math.tan(JSprite.math.rad(angle)),
+  asin: slope   => JSprite.math.deg(Math.asin(slope)),
+  acos: slope   => JSprite.math.deg(Math.acos(slope)),
+  atan: slope   => JSprite.math.deg(Math.atan(slope)),
+  mod:  (x,y)   => (x + y) % y
 };
